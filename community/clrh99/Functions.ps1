b39e25d05ce9e3ae13f45e198289be48ccbab9e0
stknohg <stknohg@gmail.com>
2016-04-20 18:15:55 +0900

    added sample for clrh99

---
+++ community/clrh99/Functions.ps1
@@ -0,0 +1,44 @@
+﻿#
+# ファンクションのサンプル
+#
+function WriteHello([string]$Message)
+{
+    return Write-Output ("Hello {0}!" -f $Message)
+}
+
+#
+# スクリプトコマンドレットのサンプル
+#
+function Write-Hello
+{
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Message
+    )
+    Begin
+    {
+        Write-Verbose "Begin function..."
+    }
+    Process
+    {
+        Write-Verbose ("Process input value {0}..." -f $Message)
+        return Write-Output ("Hello {0}!" -f $Message)
+    }
+    End
+    {
+        Write-Verbose "End function."
+    }
+}
+
+#
+# フィルターのサンプル
+#
+filter HelloFilter
+{
+    return Write-Output ("Hello {0}!" -f $_)
+}
---
+++ community/clrh99/Pipeline.ps1
@@ -0,0 +1,56 @@
+﻿#
+# パイプラインのテスト用コマンド1
+#
+function Command1
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, Position=0)]
+        $InputObject
+    )
+    Begin
+    {
+        Write-Host "Command1 Begin($InputObject)..." -ForegroundColor Green
+    }
+    Process
+    {
+        Write-Host "Command1 Process($InputObject)..." -ForegroundColor Green
+        Write-Output $InputObject
+    }
+    End
+    {
+        Write-Host "Command1 End($InputObject)." -ForegroundColor Green
+    }
+}
+
+#
+# パイプラインのテスト用コマンド2
+#
+function Command2
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true, ValueFromPipeline=$true, Position=0)]
+        $InputObject
+    )
+    Begin
+    {
+        Write-Host "Command2 Begin($InputObject)..." -ForegroundColor Yellow
+    }
+    Process
+    {
+        Write-Host "Command2 Process($InputObject)..." -ForegroundColor Yellow
+        Write-Output $InputObject
+    }
+    End
+    {
+        Write-Host "Command2 End($InputObject)." -ForegroundColor Yellow
+    }
+}
+
+#
+# パイプラインのテスト
+#
+@(1..10) | Command1 | Command2
