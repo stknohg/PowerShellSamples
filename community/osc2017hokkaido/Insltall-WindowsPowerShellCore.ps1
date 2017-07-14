aab2b14d9512070950b941f0765485fe128f6840
stknohg <stknohg@gmail.com>
2017-07-14 10:52:14 +0900

    PowerShell Core Beta 4.に対応

--- community/osc2017hokkaido/Insltall-WindowsPowerShellCore.ps1
+++ community/osc2017hokkaido/Insltall-WindowsPowerShellCore.ps1
@@ -1,8 +1,8 @@
 # PowerShell(MSI)
 Invoke-WebRequest `
-    -Uri "https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.3/PowerShell-6.0.0-beta.3-win10-win2016-x64.msi" `
-    -OutFile "$($pwd.Path)\PowerShell-6.0.0-beta.3.msi“
+    -Uri "https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-beta.4/PowerShell-6.0.0-beta.4-win10-win2016-x64.msi" `
+    -OutFile "$($pwd.Path)\PowerShell-6.0.0-beta.4.msi"
 Start-Process `
     -FilePath msiexec.exe `
-    -ArgumentList @("/i", "$($pwd.Path)\PowerShell-6.0.0-beta.3.msi", "/passive") `
+    -ArgumentList @("/i", "$($pwd.Path)\PowerShell-6.0.0-beta.4.msi", "/passive") `
     -Wait -PassThru
